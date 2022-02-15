fb8e00272ffa0d9545c4adc87e25ccb39195f4e6
Laura Iolanda Ruas Fernandes <lauraifernandes2@gmail.com>
2022-02-15 14:51:07 -0300

    Iniciando o GIT

---
+++ js/index.js
@@ -0,0 +1,149 @@
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+
+
+checkLogged();
+
+
+
+// Logar no sistema
+
+document.getElementById("login-form").addEventListener("submit", function (e){
+
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checksession = document.getElementById("session-check").checked;
+    const account = getAccount(email);
+
+    if(!account){
+
+        alert("Opps! Verifique o e-mail ou a senha.");
+        return;
+    }
+    if (account)
+    {
+
+        if(account.password !== password)
+        {
+
+            alert("Opps! Verifique o e-mail ou a senha.");
+            return;
+        }
+
+        saveSession(email, checksession);
+
+        window.location.href="home.html";
+
+    }
+
+
+});
+
+
+
+// Criar uma conta
+
+
+document.getElementById("create-form").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    //alert(password, email);
+    //console.log(email, password);
+
+    if(email.length < 5)
+    {
+
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+
+    }
+
+    if(password.length < 4)
+    {
+
+        alert("Preencha o campo com uma senha de no mínimo 4 dígitos.");
+        return;
+
+    }
+
+    saveAccount({
+
+        login: email,
+        password: password,
+        transactions: []
+
+    });
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso");
+
+    
+});
+
+function checkLogged()
+{
+
+    if(session){
+
+    sessionStorage.setItem("logged", session);
+
+    logged = session;
+
+    }
+
+    if(logged)
+    {
+
+        saveSession(logged, session);
+
+        window.location.href = "home.html";
+
+
+    }
+
+}
+
+function saveAccount(data)
+{
+
+    localStorage.setItem(data.login, JSON.stringify(data));
+
+
+}
+
+function saveSession(data, saveSession){
+
+    if (saveSession)
+    {
+
+        localStorage.setItem("session", data);
+
+    }
+
+    sessionStorage.setItem("logged", data);
+
+}
+
+function getAccount (key)
+{
+
+    const account = localStorage.getItem(key);
+
+    if(account)
+    {
+
+        return JSON.parse(account);
+
+    }
+
+    return "";
+}
